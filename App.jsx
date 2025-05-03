7090e44f6c3eb932d7696f96e29014d0d9ae468a
Nishi Surti <nishisurti01@gmail.com>
2025-05-03 12:56:18 -0400

    solve challange #2

--- App.jsx
+++ App.jsx
@@ -1,13 +1,17 @@
 import padData from "./pads"
 import { useState } from "react"
 
-export default function App() {
+export default function App(props) {
 
     const [pad,setPad]= useState(padData)
+    const styles = {
+        backgroundColor : props.darkMode ? "#222222" : "#cccccc"  
+    }
     return (
         <main>
             <div className="pad-container">
-                {pad.map((eachPad) => <button key={eachPad.id}></button>)}
+                {/* always add key when mapping */}
+                {pad.map((eachPad) => <button style={styles} key={eachPad.id}></button>)} 
             </div>
         </main>
     )
